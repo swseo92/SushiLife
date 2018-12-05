bug fix - overflow error at "cash_earned" in "Agent.py"

SushiLife/Account.py
@@ -88,10 +88,10 @@ class StockAccount(AssetAccount):
                     print("매수 실패 : ", names[i], "주문가", 주문가격[i], "주문수량 : ", 주문수량[i])
 
         names = np.array(names)[체결]
-        체결가, 현재가 = 체결가[체결], 현재가[체결]
+        체결가, 현재가 = 체결가[체결].astype("float64"), 현재가[체결]
         주문수량 = np.array(주문수량)[체결]
 
-        self._add_assets(names, 체결가, 현재가, 주문수량)
+        self._add_assets(names[주문수량 > 0], 체결가[주문수량 > 0], 현재가[주문수량 > 0], 주문수량[주문수량 > 0])
 
         거래대금 = np.sum(체결가 * 주문수량)
         return 거래대금
@@ -114,16 +114,17 @@ class StockAccount(AssetAccount):
                     print("매도 실패 : ", names[i], "주문가 : ", 주문가격[i], "주문수량 : ", 주문수량[i])
 
         names = np.array(names)[체결]
-        체결가 = 체결가[체결]
+        체결가 = 체결가[체결].astype("float64")
         주문수량 = np.array(주문수량)[체결]
-        self._remove_assets(names, 체결가, 주문수량)
+        self._remove_assets(names[주문수량 > 0], 체결가[주문수량 > 0], 주문수량[주문수량 > 0])
 
-        거래대금 = np.sum(체결가 * 주문수량) * (100 - self._tax - self._fee - self._slippage) / 100
+        거래대금 = np.sum((체결가 * 주문수량 * (100 - self._tax - self._fee - self._slippage) / 100).astype("int64"))
         return 거래대금
 
     def _get_current_price(self):
         names = list(self.keys())
-        current_price = self._exchange.get_assets_info(codes=names, fields=["현재가", "대비"])
+        # current_price = self._exchange.get_assets_info(codes=names, fields=["현재가", "대비"])
+        current_price = self._exchange._DataAsset.get_info(self._date, num=1, codes=names, fields=["현재가", "대비"]).reshape(-1, 2)
 
         return names, current_price
 

SushiLife/Agent.py
@@ -11,20 +11,57 @@ class Agent:
         self.total_balance = initial_cash
 
         self.accounts = dict()  # 여러 자산군에 대한 계좌
+        self._basket = dict()  # 해당일에 구매리스트를 받기위한 바구니
 
         self.report = {"수익률(%)": [], "누적수익률(%)": [], "총자산(원)": [],
                        "CAGR(%)": [], "일평균수익률(%)": [], "MDD": [], "최대수익률(%)": [], "날짜": []}
 
     def set_account(self, name, account):
-        self.accounts[name] = account
+        self.accounts[name] = account  # 계좌등록
+        # 해당 자산에 대해서 바스켓 생성
+        self._basket[name] = {"구매이름": [], "구매가격": [], "구매수량": [], "구매종류": [], "구매시간": [],
+                              "판매이름": [], "판매가격": [], "판매수량": [], "판매종류": [], "판매시간": []}
 
-    def buy(self, name_account, names, 주문가격, 주문수량, **kwds):
+    def buy(self, name_account, name, 주문가격, 주문수량, 주문종류="limit", 주문시간="장중"):
+        self._basket[name_account]["구매이름"].append(name)
+        self._basket[name_account]["구매가격"].append(주문가격)
+        self._basket[name_account]["구매수량"].append(주문수량)
+        self._basket[name_account]["구매종류"].append(주문종류)
+        self._basket[name_account]["구매시간"].append(주문시간)
+
+    def sell(self, name_account, name, 주문가격, 주문수량, 주문종류="limit", 주문시간="장중"):
+        self._basket[name_account]["판매이름"].append(name)
+        self._basket[name_account]["판매가격"].append(주문가격)
+        self._basket[name_account]["판매수량"].append(주문수량)
+        self._basket[name_account]["판매종류"].append(주문종류)
+        self._basket[name_account]["판매시간"].append(주문시간)
+
+    def _reset_basket(self, name):
+        self._basket[name] = {"구매이름": [], "구매가격": [], "구매수량": [], "구매종류": [], "구매시간": [],
+                              "판매이름": [], "판매가격": [], "판매수량": [], "판매종류": [], "판매시간": []}
+
+    def _shopping_basket(self):
+        for name_account in self.accounts.keys():
+            self._sell_list(name_account, self._basket[name_account]["판매이름"],
+                      self._basket[name_account]["판매가격"], self._basket[name_account]["판매수량"],
+                      주문종류=self._basket[name_account]["판매종류"], 주문시간=self._basket[name_account]["판매시간"])
+
+            self._buy_list(name_account, self._basket[name_account]["구매이름"],
+                     self._basket[name_account]["구매가격"], self._basket[name_account]["구매수량"],
+                     주문종류=self._basket[name_account]["구매종류"], 주문시간=self._basket[name_account]["구매시간"])
+
+            self._reset_basket(name_account)
+
+    def _buy_list(self, name_account, names, 주문가격, 주문수량, 주문종류=None, 주문시간=None):
+        if len(names) == 0:
+            return False
         주문가격 = np.array(주문가격)
         주문수량 = np.array(주문수량)
 
         cash_required = np.sum(주문가격 * 주문수량)
+
         if cash_required > self.cash:
-            idx = np.sum(np.cumsum(주문가격 * 주문수량) < cash_required) - 1
+            idx = np.sum(np.cumsum(주문가격 * 주문수량) < self.cash) - 1
             if idx == -1:
                 return False
 
@@ -32,22 +69,30 @@ class Agent:
             주문가격 = 주문가격[:idx]
             주문수량 = 주문수량[:idx]
 
-        cash_consumed = self.accounts[name_account].buy(names, 주문가격, 주문수량, **kwds)
+            if 주문종류 is not None:
+                주문종류 = 주문종류[:idx]
+            if 주문시간 is not None:
+                주문시간 = 주문시간[:idx]
+
+        cash_consumed = self.accounts[name_account].buy(names, 주문가격, 주문수량, 주문종류=주문종류, 주문시간=주문시간)
         self.cash -= cash_consumed
 
         return True
 
-    def sell(self, name_account, names, 주문가격, 주문수량, **kwds):
+    def _sell_list(self, name_account, names, 주문가격, 주문수량, **kwds):
+        if len(names) == 0:
+            return False
         주문가격 = np.array(주문가격)
         주문수량 = np.array(주문수량)
 
         cash_earned = self.accounts[name_account].sell(names, 주문가격, 주문수량, **kwds)
         self.cash += cash_earned
 
-    def update_date(self, date):
-        self._date = date
 
+    def update_date(self, date):
+        self._shopping_basket()
         self.total_balance = 0
+
         for key in self.accounts.keys():
             self.accounts[key].update_from_agent(date)
             self.total_balance += self.accounts[key].get_total_balance()
@@ -55,47 +100,37 @@ class Agent:
         self.total_balance += self.cash
         self._update_report()
 
-    def reset(self, date):
         self._date = date
 
-        for key in self.accounts.keys():
-            self.accounts[key].reset_from_agent(date)
-
-        self.cash = self._initial_cash * 1
-        self.total_balance = self._initial_cash * 1
-
-        self.report = {"수익률(%)": [], "누적수익률(%)": [], "총자산(원)": [],
-                       "CAGR(%)": [], "일평균수익률(%)": [], "MDD": [], "최대수익률(%)": [], "날짜": []}
-        self._update_report()
+    def init(self, date):
+        self._date = date
+        self.report = {"수익률(%)": [0.0], "누적수익률(%)": [0.0], "총자산(원)": [self._initial_cash],
+                       "CAGR(%)": [0.0], "일평균수익률(%)": [0.0], "MDD": [0.0], "최대수익률(%)": [0], "날짜": [self._date]}
 
     def _update_report(self):
-        if not self.report["날짜"]:
-            self.report = {"수익률(%)": [0.0], "누적수익률(%)": [0.0], "총자산(원)": [self._initial_cash],
-                           "CAGR(%)": [0.0], "일평균수익률(%)": [0.0], "MDD": [0.0], "최대수익률(%)": [0], "날짜": [self._date]}
-        else:
-            당일수익률 = (self.total_balance - self.report["총자산(원)"][-1]) / self.report["총자산(원)"][-1] * 100
-            누적수익률 = (self.total_balance / self._initial_cash - 1) * 100
-            총자산 = self.total_balance
-            경과 = (self._date - self.report["날짜"][0]).days + 1
+        당일수익률 = (self.total_balance - self.report["총자산(원)"][-1]) / self.report["총자산(원)"][-1] * 100
+        누적수익률 = (self.total_balance / self._initial_cash - 1) * 100
+        총자산 = self.total_balance
+        경과 = (self._date - self.report["날짜"][0]).days + 1
 
-            일평균수익률 = ((누적수익률 / 100 + 1) ** (1 / 경과) - 1) * 100
-            CAGR = ((일평균수익률 / 100 + 1) ** 365 - 1) * 100
+        일평균수익률 = ((누적수익률 / 100 + 1) ** (1 / 경과) - 1) * 100
+        CAGR = ((일평균수익률 / 100 + 1) ** 365 - 1) * 100
 
-            최대수익률 = max(누적수익률, np.max(self.report["누적수익률(%)"]))
-            DD = (누적수익률 - 최대수익률) / (최대수익률 + 100) * 100
-            MDD = min(DD, np.min(self.report["MDD"]))
+        최대수익률 = max(누적수익률, np.max(self.report["누적수익률(%)"]))
+        DD = (누적수익률 - 최대수익률) / (최대수익률 + 100) * 100
+        MDD = min(DD, np.min(self.report["MDD"]))
 
-            레포트_당일 = {"수익률(%)": 당일수익률, "누적수익률(%)": 누적수익률, "총자산(원)": 총자산,
-                      "CAGR(%)": CAGR, "일평균수익률(%)": 일평균수익률, "MDD": MDD, "최대수익률(%)": 최대수익률, "날짜": self._date}
+        레포트_당일 = {"수익률(%)": 당일수익률, "누적수익률(%)": 누적수익률, "총자산(원)": 총자산,
+                  "CAGR(%)": CAGR, "일평균수익률(%)": 일평균수익률, "MDD": MDD, "최대수익률(%)": 최대수익률, "날짜": self._date}
 
-            for field in self.report.keys():
-                self.report[field].append(레포트_당일[field])
+        for field in self.report.keys():
+            self.report[field].append(레포트_당일[field])
 
-            if self._출력:
-                print("장마감 : ", self._date, "\n\n",
-                      "당일수익률(%) : ", 당일수익률, "\n",
-                      "누적수익률(%) : ", 누적수익률, "\n",
-                      "CAGR(%)", CAGR, "\n",
-                      "MDD : ", MDD, "\n",
-                      "총자산(원) : ", 총자산, "\n",
-                      "--------------------------------------------------\n")
+        if self._출력:
+            print("장마감 : ", self._date, "\n\n",
+                  "당일수익률(%) : ", 당일수익률, "\n",
+                  "누적수익률(%) : ", 누적수익률, "\n",
+                  "CAGR(%)", CAGR, "\n",
+                  "MDD : ", MDD, "\n",
+                  "총자산(원) : ", 총자산, "\n",
+                  "--------------------------------------------------\n")

SushiLife/DataAsset.py
@@ -18,7 +18,6 @@ def make_data(filename, name, data_df, fields=None, dtype=None, dates=None):
         dates = dates
 
     codes = list(data_df.keys())
-
     list_data = list()
 
     fields = list(data_df[codes[0]].columns)
@@ -33,12 +32,22 @@ def make_data(filename, name, data_df, fields=None, dtype=None, dates=None):
     array = np.concatenate(list_data, axis=1)
 
     if dtype == "stock":
-        array[:, :, -1][np.where(array[:, :, -1] == "코스피")] = 0
-        array[:, :, -1][np.where(array[:, :, -1] == "코스닥")] = 1
+
+        idx = fields.index("시장구분")
+
+        array[:, :, idx][np.where(array[:, :, idx] == "코스피")] = 0
+        array[:, :, idx][np.where(array[:, :, idx] == "코스닥")] = 1
         array = array.astype('f')
 
     f = h5py.File(filename, "a")
-    f.create_dataset(name, data=array)
+    try:
+        f.create_dataset(name, data=array)
+    except:
+        del f[name]
+        f.close()
+
+        f = h5py.File(filename, "a")
+        f.create_dataset(name, data=array)
     f.close()
 
     with open("%s-%s.axis" % (filename, name), "wb") as f:
@@ -58,18 +67,11 @@ def load_data(file, name, chunks=5, in_memory=False):
     return array, axis
 
 class DataAsset:
-    def __init__(self, array, axis, chunks=300):
+    def __init__(self, array, axis):
         #         array, axis = make_data(data_df)
 
-        self.dates, self.codes, self.fields = axis
-
+        self.dates, self.codes, self.fields = list(axis[0]), list(axis[1]), list(axis[2])
         self.array = array
-
-        self._chunks = chunks
-        self._dates_chunk = []
-
-        self._date = None
-        self.df_date = None
 
     def get_info(self, date, num=1, codes=None, fields=None):
         """
@@ -80,8 +82,11 @@ class DataAsset:
         :param fields: list, 반환할 필드들의 리스트
         :return: numpy.array
         """
-        idx_date = self._dates_chunk.index(date)
-        array = self._array_chunk[max(0, idx_date - num + 1):idx_date + 1]
+        # if date not in self._dates_chunk:
+        #     self._make_chunk(date)
+
+        idx_date = self.dates.index(date)
+        array = self.array[max(0, idx_date - num + 1):idx_date + 1]
 
         if codes is not None:
             idx_codes = [self.codes.index(code) for code in codes]
@@ -95,15 +100,8 @@ class DataAsset:
 
         return array.compute()
 
-    def _make_chunk(self):
-        idx_date = self.dates.index(self._date)
-        self._dates_chunk = self.dates[max(0, idx_date - self._chunks):idx_date + self._chunks]
-        self._array_chunk = self.array[max(0, idx_date - self._chunks):idx_date + self._chunks]
-
     def update_date(self, date):
-        self._date = date
-        if date not in self._dates_chunk:
-            self._make_chunk()
+        pass
 
-    def reset(self, date):
+    def init(self, date):
         self.update_date(date)

SushiLife/Exchnage.py
@@ -13,7 +13,7 @@ class Exchange:
     def buy(self, list_codes, 주문가격, 주문종류=None, 주문시간=None):
         OCLHVVM = self.get_assets_info(codes=list_codes)
         market_type = OCLHVVM[:, -1]
-        OCLHVVM = OCLHVVM[:, :-2].astype("int64")
+        OCLHVVM = OCLHVVM[:, :-2]
 
         주문가격 = cal_price_tick_unit(주문가격, market_type)
 
@@ -28,8 +28,7 @@ class Exchange:
             주문시간 = np.array(주문시간)
 
         주문시간 = np.array(주문시간)
-
-        체결가 = np.zeros_like(list_codes, dtype="int") * np.nan  # 체결가가 nan인 경우 미체결, 숫자인 경우 체결가격
+        체결가 = np.zeros_like(주문가격) * np.nan  # 체결가가 nan인 경우 미체결, 숫자인 경우 체결가격
 
         # 체결조건 (시간순서로)
         cond1 = (OCLHVVM[:, 4] == 0) | (np.isnan(OCLHVVM[:, 4]))  # 미체결, 거래량이 0이거나 상장되지 않음
@@ -54,7 +53,7 @@ class Exchange:
     def sell(self, list_codes, 주문가격, 주문종류=None, 주문시간=None):
         OCLHVVM = self.get_assets_info(codes=list_codes)
         market_type = OCLHVVM[:, -1]
-        OCLHVVM = OCLHVVM[:, :-2].astype("int64")
+        OCLHVVM = OCLHVVM[:, :-2]
 
         주문가격 = cal_price_tick_unit(주문가격, market_type)
 
@@ -70,10 +69,10 @@ class Exchange:
 
         주문시간 = np.array(주문시간)
 
-        체결가 = np.zeros_like(list_codes, dtype="int") * np.nan  # 체결가가 nan인 경우 미체결, 숫자인 경우 체결가격
+        체결가 = np.zeros_like(주문가격) * np.nan  # 체결가가 nan인 경우 미체결, 숫자인 경우 체결가격
 
         # 체결조건 (시간순서로)
-        cond1 = (OCLHVVM[:, 4] == 0) | (np.isnan(OCLHVVM[:, 4]))  # 미체결, 거래량이 0이거나 상장되지 않음
+        cond1 = (OCLHVVM[:, 4] == 0) | (np.isnan(OCLHVVM[:, 1]))  # 미체결, 거래량이 0이거나 상장되지 않음
         cond2 = (주문시간 == "장전") & (OCLHVVM[:, 0] >= 주문가격)  # 체결, 장 시작과 동시에 체결
         cond3 = OCLHVVM[:, 3] >= 주문가격  # 체결, 장중 체결 : 저가 > 판매가
         cond4 = OCLHVVM[:, 2] >= 주문가격  # 체결, 장중 체결 : 고가 > 판매가
@@ -112,7 +111,7 @@ class Exchange:
         self._date = date
         self._get_OCLHVV()
 
-    def reset(self, date):
+    def init(self, date):
         self.update_date(date)
 
     def _get_OCLHVV(self):
@@ -131,7 +130,7 @@ class Exchange:
 def cal_price_tick_unit(price, market_type):
     price = np.array(price)
     market_type = np.array(market_type).reshape(-1)
-    tick_unit = np.zeros_like(price)
+    tick_unit = np.zeros_like(price, dtype="float64")
 
     kosdaq = (market_type == 1)  # type : 0: 코스피, 1: 코스닥
     tick_unit[kosdaq] = 100
@@ -148,5 +147,5 @@ def cal_price_tick_unit(price, market_type):
     tick_unit[~kosdaq & (price < 5000)] = 5
     tick_unit[~kosdaq & (price < 1000)] = 1
 
-    price = ((price / tick_unit) * tick_unit).astype("i")
+    price = np.floor(price / tick_unit) * tick_unit
     return price